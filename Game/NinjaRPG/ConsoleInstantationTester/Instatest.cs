af8c93d15f8e3f9f328e176937811caf1ebe91bb
rusekov <rusekov@gmail.com>
2014-02-15 12:49:44 +0200

    Adding test project "Instatest" where to test all objects instantiation

---
+++ Game/NinjaRPG/ConsoleInstantationTester/Instatest.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleInstantationTester
+{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NinjaWorld;
+
+    class Instatest
+    {
+        static void Main()
+        {
+            Ninja Ninja = new Ninja("Gogo");
+            Console.WriteLine(Ninja.Cash);
+        }
+    }
+}
