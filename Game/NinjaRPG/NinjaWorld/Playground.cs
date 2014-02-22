2e6441da388b3a0d64bc70d435ba7f06cd5b511c
kreatora3 <annihilatora@gmail.com>
2014-02-22 17:34:09 +0200

    Playground created and added as Singleton

---
+++ Game/NinjaRPG/NinjaWorld/Playground.cs
@@ -0,0 +1,30 @@
+﻿namespace NinjaWorld
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public sealed class Playground : CommercialBuilding
+    {
+        private static List<ICommercial> recreationGoods = new List<ICommercial>() 
+        {
+            new Recreations("PlayingPoker"),
+            new Recreations("VideoGames"),
+            new Recreations("Bowling"),
+            new Recreations("Billiards"),
+            new Recreations("Dance")
+        }.OrderBy(rec => rec.Price).ToList();
+
+        private static readonly Lazy<Playground> PG = new Lazy<Playground>(() => new Playground());
+
+         private Playground()
+            : base("Playground", recreationGoods)
+        {
+        }
+
+         public static Playground Instance
+         {
+             get { return PG.Value; }
+         }
+    }
+}
