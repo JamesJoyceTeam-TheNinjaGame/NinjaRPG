8e4864c291218da25abde31999c687d151273b0f
rusekov <rusekov@gmail.com>
2014-02-23 04:35:50 +0200

    And some RedBul

---
+++ Game/NinjaRPG/NinjaWorld/Energizer.cs
@@ -0,0 +1,43 @@
+﻿namespace NinjaWorld
+{
+    public class Energizer : Item, ICommercial, IItem
+    {
+        private const int PricePerHeal = 1;
+        private EnergizerEnum energizer;
+
+        private int price;
+
+        public Energizer(EnergizerEnum energizingItem, string name)
+            : base(name)
+        {
+            this.energizer = energizingItem;
+            this.HealingPoints = (int)this.energizer;
+            this.Price = this.HealingPoints * PricePerHeal;
+        }
+
+        public int HealingPoints { get; private set; }
+
+        public int Price
+        {
+            get
+            {
+                return this.price;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ImproperlyDefinedItemException("Price could be only positive integer");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public object Clone()
+        {
+            return new Energizer(this.energizer, this.Name);
+        }
+    }
+}
